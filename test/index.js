4c7aa44d28c501fdda724c27765aa9397259af70
Sander Huijsen <sander.huijsen@gmail.com>
2017-10-07 11:38:48 +0800

    The test is now an actual message for an M4.

--- test/index.js
+++ test/index.js
@@ -1,30 +1,31 @@
-const assert = require('assert');
 const Enigma = require('../Enigma');
 
+// http://wiki.franklinheath.co.uk/index.php/Enigma/Sample_Messages
+// U-264 (Kapitänleutnant Hartwig Looks), 1942
+const config = {
+    "type": 4,
+    "plugBoard": ["AT", "BL", "DF", "GJ", "HM", "NW", "OP", "QY", "RZ", "VX"],
+    "reflectorType": "B",
+    "rotors": [{
+        "type": "beta",
+        "ringSetting": 1
+    }, {
+        "type": "II",
+        "ringSetting": 1
+    }, {
+        "type": "IV",
+        "ringSetting": 1
+    }, {
+        "type": "I",
+        "ringSetting": 22
+    }]
+};
+
 try {
-    const enigma = new Enigma();
-
-    let i = 0;
-    const results = [];
-    for (; i < 26; i++) {
-        const r = enigma.onKey('A');
-        results.push(r);
-    }
-    const expected = 'BDZGOWCXLTKSBTMCDLPBMUQOFX';
-    assert.equal(
-        results.join(''),
-        expected,
-        `Expected ${expected},\n     got ${results.join('')}`
-    );
-
-    enigma.reset();
-
-    const result = enigma.onMessage('AAAAAAAAAAAAAAAAAAAAAAAAAA');
-    assert.equal(
-        result,
-        expected,
-        `Expected ${expected},\n     got ${result}`
-    );
+    const enigma = new Enigma(config);
+    const result = enigma.onMessage('VJNA', `NCZWVUSXPNYMINHZXMQXSFWXWLKJAHSHNMCOCCAKUQPMKCSMHKSEINJUSBLKIOSXCKUBHMLLXCSJUSRRDVKOHULXWCCBGVLIYXEOAHXRHKKFVDREWEZLXOBAFGYUJQUKGRTVUKAMEURBVEKSUHHVOYHABCJWMAKLFKLMYFVNRIZRVVRTKOFDANJMOLBGFFLEOPRGTFLVRHOWOPBEKVWMUQFMPWPARMFHAGKXIIBG`);
+    console.log(result);
 } catch (e) {
     console.log(e.message);
+    console.log(`Error code ${e.code}`);
 }
